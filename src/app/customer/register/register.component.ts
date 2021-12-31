de808a40f12dd139b941f88ed7f5c3a2fc166985
César Calle <mcalle@ulasalle.edu.pe>
2021-12-31 15:57:45 -0500

    feat: integrate age autocalculate

--- src/app/customer/register/register.component.ts
+++ src/app/customer/register/register.component.ts
@@ -13,9 +13,26 @@ export class RegisterComponent implements OnInit {
     dateOfBirthControl: new FormControl('', [Validators.required]),
     ageControl: new FormControl({ value: '', disabled: true }, [Validators.required]),
   });
+  minDateOfBirth = '1900-01-01';
+  maxDateOfBirth = [new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate()].join('-')
   constructor() { }
 
   ngOnInit(): void {
+    this.form.get('dateOfBirthControl')?.valueChanges.subscribe(date => {
+      let dateParsed = new Date(date);
+      dateParsed.setHours(+dateParsed.getHours() + dateParsed.getTimezoneOffset() / 60)
+      this.form.get('ageControl')?.setValue(this.ageFormBirthDate(dateParsed))
+    })
+  }
+
+  ageFormBirthDate(birthDate: Date) {
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const months = today.getMonth() - birthDate.getMonth();
+    if (months < 0 || (months === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
   }
 
   onSubmit() {
