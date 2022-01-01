78d02d4e7f9a1e31f0e92cd39bcbb64ba876db17
César Calle <mcalle@ulasalle.edu.pe>
2022-01-01 10:43:57 -0500

    fix: url in enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  urlApi: 'https://llatan.cesarcalle.me:8083/'
+  urlApi: 'https://llatan.cesarcalle.me:8083'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  urlApi: 'http://localhost:10010/'
+  urlApi: 'http://localhost:10010'
 };
 
 /*
